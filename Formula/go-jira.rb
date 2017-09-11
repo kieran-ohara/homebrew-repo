9fc8c23f9cec9d60da9c1562094ce4e1589241d5
Kieran Bamforth <hello@kieranbamforth.me>
2017-09-11 13:36:39 +0100

    Updating the formula for go-jira

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -3,15 +3,15 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class GoJira < Formula
-  desc "simple jira command line client in Go"
-  homepage "https://github.com/kieran-bamforth/go-jira"
-  url "https://github.com/kieran-bamforth/go-jira/archive/kb0.1.15.tar.gz"
-  sha256 "fce5903c2c2cd0382ed5a3bc30d436df88a8f76f5b98a7c7e6ccaf68218dc360"
+	desc "simple jira command line client in Go"
+	homepage "https://github.com/Netflix-Skunkworks/go-jira"
+	url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/v1.0.5/jira-darwin-10.6-amd64"
+	sha256 "b9d975396bb6570489750a806ada87fc2a00a342cb65efe373d3d24748885bb3"
+	version "1.0.5"
 
-  depends_on "go" => :build
+	depends_on "go" => :build
 
-  def install
-    system "make", "install", "BIN=#{prefix}/bin/jira"
-  end
-
+	def install
+		bin.install "jira-darwin-10.6-amd64" => "jira"
+	end
 end
