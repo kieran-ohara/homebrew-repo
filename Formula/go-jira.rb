78e0cec390a4ae90780622d1d2a137de6f1a9ac5
Kieran Bamforth <hello@kieranbamforth.me>
2017-12-18 17:35:43 +0000

    updating the sha

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -2,7 +2,7 @@ class GoJira < Formula
 	desc "simple jira command line client in Go"
 	homepage "https://github.com/Netflix-Skunkworks/go-jira"
 	url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/v1.0.14/jira-darwin-10.6-amd64"
-	sha256 "081bde36d2b5fd4f068e01b0179bae55db99b489b77a6577a01f448f423e16b57"
+	sha256 "81bde36d2b5fd4f068e01b0179bae55db99b489b77a6577a01f448f423e16b57"
 	version "1.0.14"
 
 	depends_on "go" => :build
