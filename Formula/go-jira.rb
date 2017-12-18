0d16894396bb335f739be245f0003be11b633cbb
Kieran Bamforth <hello@kieranbamforth.me>
2017-12-18 17:30:19 +0000

    bumping up go-jira to 1.0.14

--- Formula/go-jira.rb
+++ Formula/go-jira.rb
@@ -1,13 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class GoJira < Formula
 	desc "simple jira command line client in Go"
 	homepage "https://github.com/Netflix-Skunkworks/go-jira"
-	url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/v1.0.5/jira-darwin-10.6-amd64"
-	sha256 "060f540c0a2b9bb82330ebe270d136240f9559212abbd6b0b5dde5a2d8c0fb08"
-	version "1.0.5"
+	url "https://github.com/Netflix-Skunkworks/go-jira/releases/download/v1.0.14/jira-darwin-10.6-amd64"
+	sha256 "081bde36d2b5fd4f068e01b0179bae55db99b489b77a6577a01f448f423e16b57"
+	version "1.0.14"
 
 	depends_on "go" => :build
 
