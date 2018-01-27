eeb9a1a9a567e3e02319e6fddc855969c438201b
Kieran Bamforth <hello@kieranbamforth.me>
2018-01-27 14:49:28 +0000

    depending on node again

--- Formula/client-cert-proxy.rb
+++ Formula/client-cert-proxy.rb
@@ -7,6 +7,8 @@ class ClientCertProxy < Formula
 
     plist_options :startup => true
 
+    depends_on "node"
+
     def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
