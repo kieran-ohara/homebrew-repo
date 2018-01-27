c9c1c1d239941b67b3a78d6825edb3bbd8753b8c
Kieran Bamforth <hello@kieranbamforth.me>
2018-01-27 14:48:19 +0000

    updated eslint formulae

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -3,10 +3,11 @@ require "language/node"
 class ESLint < Formula
     desc "A fully pluggable tool for identifying and reporting on patterns in JavaScript"
     homepage "https://eslint.org/"
-    url "https://github.com/eslint/eslint/archive/v4.16.0.tar.gz"
+    url "https://registry.npmjs.org/eslint/-/eslint-4.16.0.tgz"
+    sha256 "d6160ad2b2efccd7cc23a5c54a9603089e0eb3cd8737869e82657e6d89c4b21b"
 
     def install
-        system "npm", "install", "eslint@4.16.0", *Language::Node.std_npm_install_args(libexec)
+        system "npm", "install", *Language::Node.std_npm_install_args(libexec)
         bin.install_symlink Dir["#{libexec}/bin/*"]
     end
 
