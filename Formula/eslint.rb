756eeb1b684f0807e8e4bee1095089e0642859dc
Kieran Bamforth <hello@kieranbamforth.me>
2018-01-27 14:49:03 +0000

    added depends on node line

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -6,6 +6,8 @@ class ESLint < Formula
     url "https://registry.npmjs.org/eslint/-/eslint-4.16.0.tgz"
     sha256 "d6160ad2b2efccd7cc23a5c54a9603089e0eb3cd8737869e82657e6d89c4b21b"
 
+    depends_on "node"
+
     def install
         system "npm", "install", *Language::Node.std_npm_install_args(libexec)
         bin.install_symlink Dir["#{libexec}/bin/*"]
