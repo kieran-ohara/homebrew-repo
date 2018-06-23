8aa4b959b6ca854c2147126b12074f49659dade7
Kieran Bamforth <hello@kieranbamforth.me>
2018-06-23 19:19:00 +0100

    Changing back to GitHubPrivateRepositoryReleaseDownloadStrategy
    Signed-off-by: Kieran Bamforth <hello@kieranbamforth.me>

--- Formula/awscli-wormhole-credentials.rb
+++ Formula/awscli-wormhole-credentials.rb
@@ -1,7 +1,7 @@
 class AwscliWormholeCredentials < Formula
   desc "Use AWS CLI with BBC AWS accounts. "
   homepage "https://github.com/bbc/awscli-wormhole-credentials"
-  url "https://github.com/bbc/awscli-wormhole-credentials/archive/1.0.1.tar.gz", :using => GitHubPrivateRepositoryDownloadStrategy
+  url "https://github.com/bbc/awscli-wormhole-credentials/archive/1.0.1.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
   sha256 "91974702f1938359316fc95d8edf2852797338710f3ba140ab75afe74df6f499"
 
   depends_on "httpie"
