7dedea0766120b6aa893266ffaf73ff6770f90ff
Kieran Bamforth <hello@kieranbamforth.me>
2018-06-23 19:42:48 +0100

    Fixing shasum for awscli credentials.
    Signed-off-by: Kieran Bamforth <hello@kieranbamforth.me>

--- Formula/awscli-wormhole-credentials.rb
+++ Formula/awscli-wormhole-credentials.rb
@@ -2,7 +2,7 @@ class AwscliWormholeCredentials < Formula
   desc "Use AWS CLI with BBC AWS accounts. "
   homepage "https://github.com/bbc/awscli-wormhole-credentials"
   url "https://github.com/bbc/awscli-wormhole-authentication/releases/download/1.0.1/awscli-wormhole-credentials.sh", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "91974702f1938359316fc95d8edf2852797338710f3ba140ab75afe74df6f499"
+  sha256 "c86d9b954a6c5623671daf1f4a1a2daee98ee731efc77f8d1b700a4d6ca7d972"
 
   depends_on "httpie"
   depends_on "jq"
