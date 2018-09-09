c8b3d0b6adc18e250dd0f8e0a2a89c22ad1a6ec5
Kieran Bamforth <hello@kieranbamforth.me>
2018-09-09 15:34:02 +0100

    Adding plist to latest version of javascript-typescript-langserver to start serve.
    Signed-off-by: Kieran Bamforth <hello@kieranbamforth.me>

--- Formula/javascript-typescript-langserver.rb
+++ Formula/javascript-typescript-langserver.rb
@@ -1,15 +1,41 @@
 require "language/node"
 
 class JavascriptTypescriptLangserver < Formula
-    desc "JavaScript and TypeScript code intelligence through the Language Server Protocol"
-    homepage "https://github.com/sourcegraph/javascript-typescript-langserver"
-    url "https://registry.npmjs.org/javascript-typescript-langserver/-/javascript-typescript-langserver-2.9.4.tgz"
-    sha256 "2badfc01a741f506fafb3e220bf1192bda75da77409fd14bae4b79e5ade4c5f4"
+  version ""
+  desc "JavaScript and TypeScript code intelligence through the Language Server Protocol"
+  homepage "https://github.com/sourcegraph/javascript-typescript-langserver"
+  url "https://registry.npmjs.org/javascript-typescript-langserver/-/javascript-typescript-langserver-2.11.1.4.tgz"
+  sha256 "f20110205a67342a6ee86e85e709815d7d79a7374e7b044a386b65c7379818b"
 
-    depends_on "node"
+  plist_options :startup => true
 
-    def install
-        system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-        bin.install_symlink Dir["#{libexec}/bin/*"]
-    end
+  depends_on "node"
+
+  def plist; <<~EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+  <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+          <string>/usr/local/bin/node</string>
+          <string>/usr/local/bin/javascript-typescript-langserver</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>StandardErrorPath</key>
+      <string>/tmp/javascript-typescript-langserver.stderr</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/javascript-typescript-langserver.stdout</string>
+  </dict>
+  </plist>
+  EOS
+  end
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
 end
