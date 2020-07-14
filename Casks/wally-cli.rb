94c937286e47064a999cf4d791a6d8c74b999cea
Kieran Bamforth <hello@kieranbamforth.me>
2020-07-14 19:50:21 +0100

    Remove the wally-cli as upstream is available.

--- Casks/wally-cli.rb
+++
@@ -1,8 +0,0 @@
-cask 'wally-cli' do
-  desc "The Flash(ing tool)"
-  homepage "https://ergodox-ez.com/pages/wally"
-  version "1.1.3"
-  sha256 "3e6327f23743e3163a7bed4130f73830ecaeea400098003354ec8c63e0010095"
-  url "https://github.com/zsa/wally/releases/download/#{version}-osx/wally-osx-#{version}.dmg"
-  app 'Wally.app'
-end
