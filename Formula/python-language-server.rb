d88f5b5d0b4e32b043cd8b801282f5f83df7b364
Kieran Bamforth <hello@kieranbamforth.me>
2020-09-04 22:00:31 +0100

    Update python-language-server to 0.34.1.

--- Formula/python-language-server.rb
+++ Formula/python-language-server.rb
@@ -1,11 +1,11 @@
 class PythonLanguageServer < Formula
   desc "An implementation of the Language Server Protocol for Python"
   homepage "https://github.com/palantir/python-language-server"
-  url "https://github.com/palantir/python-language-server/archive/0.21.2.tar.gz"
-  sha256 "467e4f5e67b1068fe6af6015a13dedb73863968fce4671f36ba02ae18499ca35"
-  version "0.21.2"
-
+  url "https://github.com/palantir/python-language-server/archive/0.34.1.tar.gz"
+  sha256 "e340614e2fb8428a5e12ebd40fa017832fb34627b43cab8d14b18fe6aced870c"
+  version "0.34.1"
   plist_options :startup => true
+  depends_on "python"
 
   def plist; <<~EOS
   <?xml version="1.0" encoding="UTF-8"?>
@@ -34,44 +34,34 @@ class PythonLanguageServer < Formula
 
   include Language::Python::Virtualenv
 
-  resource "configparser" do
-    url "https://files.pythonhosted.org/packages/7c/69/c2ce7e91c89dc073eb1aa74c0621c3eefbffe8216b3f9af9d3885265c01c/configparser-3.5.0.tar.gz"
-    sha256 "5308b47021bc2340965c371f0f058cc6971a04502638d4244225c49d80db273a"
-  end
-
-  resource "future" do
-    url "https://files.pythonhosted.org/packages/00/2b/8d082ddfed935f3608cc61140df6dcbf0edea1bc3ab52fb6c29ae3e81e85/future-0.16.0.tar.gz"
-    sha256 "e39ced1ab767b5936646cedba8bcce582398233d6a627067d4c6a454c90cfedb"
-  end
-
-  resource "futures" do
-    url "https://files.pythonhosted.org/packages/1f/9e/7b2ff7e965fc654592269f2906ade1c7d705f1bf25b7d469fa153f7d19eb/futures-3.2.0.tar.gz"
-    sha256 "9ec02aa7d674acb8618afb127e27fde7fc68994c0437ad759fa094a574adb265"
-  end
-
   resource "jedi" do
-    url "https://files.pythonhosted.org/packages/49/2f/cdfb8adc8cfc9fc2e5673e724d9b9098619dc1a2772cc6b8af34c6b7bef9/jedi-0.12.1.tar.gz"
-    sha256 "b409ed0f6913a701ed474a614a3bb46e6953639033e31f769ca7581da5bd1ec1"
+    url "https://files.pythonhosted.org/packages/39/67/50d1653038dafe06ca2cc55c4598c5f8318d519c12a7a288d7826280ee22/jedi-0.17.2.tar.gz"
+    sha256 "86ed7d9b750603e4ba582ea8edc678657fb4007894a12bcf6f4bb97892f31d20"
   end
 
   resource "parso" do
-    url "https://files.pythonhosted.org/packages/46/31/60de7c9cbb97cac56b193a5b61a1fd4d21df84843a570b370ec34781316b/parso-0.3.1.tar.gz"
-    sha256 "35704a43a3c113cce4de228ddb39aab374b8004f4f2407d070b6a2ca784ce8a2"
+    url "https://files.pythonhosted.org/packages/40/01/e0b8d2168fb299af90a78a5919257f821e5c21399bf0906c14c9e573db3f/parso-0.7.1.tar.gz"
+    sha256 "caba44724b994a8a5e086460bb212abc5a8bc46951bf4a9a1210745953622eb9"
   end
 
   resource "pluggy" do
-    url "https://files.pythonhosted.org/packages/a1/83/ef7d976c12d67a5c7a5bc2a47f0501c926cabae9d9fcfdc26d72abc9ba15/pluggy-0.7.1.tar.gz"
-    sha256 "95eb8364a4708392bae89035f45341871286a333f749c3141c20573d2b3876e1"
+    url "https://files.pythonhosted.org/packages/f8/04/7a8542bed4b16a65c2714bf76cf5a0b026157da7f75e87cc88774aa10b14/pluggy-0.13.1.tar.gz"
+    sha256 "15b2acde666561e1298d71b523007ed7364de07029219b604cf808bfa1c765b0"
   end
 
   resource "python-jsonrpc-server" do
-    url "https://files.pythonhosted.org/packages/d6/d7/3313dad29ff151e738d1804f496c0f93e83ee5cc35cb8dee17e44588845d/python-jsonrpc-server-0.0.1.tar.gz"
-    sha256 "ebebaa68b732c9d0aa024ffba2c3baca8b5704530f81d5a894a1ab6981b492c7"
+    url "https://files.pythonhosted.org/packages/0b/ed/ce08d4ba6048a54935797f1fcc63ce30135475cfc858778c286a0be21433/python-jsonrpc-server-0.3.4.tar.gz"
+    sha256 "c73bf5495c9dd4d2f902755bedeb6da5afe778e0beee82f0e195c4655352fe37"
   end
 
   resource "python-language-server" do
-    url "https://files.pythonhosted.org/packages/9f/1d/2817b5dc2dd77f897410a11c1c9e2a6d96b3273c53d4219dd9edab7882af/python-language-server-0.21.2.tar.gz"
-    sha256 "fa9162acb1402b807132d7288b7f521db2bd666d63505d8a4d9464d4b8488c52"
+    url "https://files.pythonhosted.org/packages/5b/d2/9a2c8fbafad6fc0e8cfc110010512a2248a773f7e94573ed7899d2b8034a/python-language-server-0.34.1.tar.gz"
+    sha256 "b96ff466b5aa24e212493de863899298f229a9e250e7353972563c7f2495d23d"
+  end
+
+  resource "ujson" do
+    url "https://files.pythonhosted.org/packages/16/c4/79f3409bc710559015464e5f49b9879430d8f87498ecdc335899732e5377/ujson-1.35.tar.gz"
+    sha256 "f66073e5506e91d204ab0c614a148d5aa938bdbf104751be66f8ad7a222f5f86"
   end
 
   def install
